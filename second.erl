b99cd29b0645b2d5f48172e092f0f555109cc05f
Jason-Carter <jjcarter@me.com>
2017-02-21 21:58:35 +0000

    Exercises 1.9 from Erlang Course done

---
+++ second.erl
@@ -0,0 +1,14 @@
+-module(second).
+-export([hypotenuse/2, perim/2, area/2]).
+
+hypotenuse(Side1, Side2) ->
+    math:sqrt(first:square(Side1) + first:square(Side2)).
+
+perim(Side1, Side2) ->
+    Side3 = hypotenuse(Side1, Side2),
+    Side1 + Side2 + Side3.
+
+area(Side1, Side2) ->
+    % Should have used first:mult...
+    %(Side1 * Side2) / 2.
+    first:mult(Side1, Side2) / 2.
