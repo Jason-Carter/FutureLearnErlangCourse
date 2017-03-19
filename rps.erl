3c27db7d90ace46fbdbbf830fe489961769feb5f
Jason-Carter <jjcarter@me.com>
2017-03-19 20:26:07 +0000

    Further progress with course

--- rps.erl
+++ rps.erl
@@ -31,11 +31,26 @@ tournament(Left, Right) -> lists:foldr(fun(X,Y) -> X+Y end, 0,
                                        lists:map(fun resultpoint/1, 
                                                  lists:zipwith(fun result/2, Left, Right))).
 
-resultpoint(Result) ->
-    % converts a win, lose or draw to an integer representation
-    case Result of
-        win -> 1;
-        draw -> 0;
-        lose -> -1
-    end.
+% Note: could have used lists:sum rather than lists:foldr
 
+% This would be better converted to function head pattern matching rather than a case statement
+% resultpoint(Result) ->
+%     % converts a win, lose or draw to an integer representation
+%     case Result of
+%         win -> 1;
+%         draw -> 0;
+%         lose -> -1
+%     end.
+resultpoint(win)  -> 1;
+resultpoint(draw) -> 0;
+resultpoint(lose) -> -1.
+
+% transform 0, 1, 2 to rock, paper, scissors and vice versa
+
+enum(0) -> rock;
+enum(1) -> paper;
+enum(2) -> scissors.
+
+val(rock)     -> 0;
+val(paper)    -> 1;
+val(scissors) -> 2.
